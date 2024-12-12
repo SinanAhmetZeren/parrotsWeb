6ed656b294ec511ef7d1edb22fb88a124a6294bb
Ahmet Zeren <sinanahmetzeren@gmail.com>
2024-12-12 23:20:05 +0300

    cluster + infowindow working

--- src/components/MainPageClusteredParrots.jsx
+++ src/components/MainPageClusteredParrots.jsx
@@ -2,6 +2,12 @@ import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { InfoWindow, useMap } from "@vis.gl/react-google-maps";
 import { MarkerClusterer } from "@googlemaps/markerclusterer";
 import { MarkerWithInfoWindow } from "./MainPageMarkerWithInfoWindow";
+import parrotMarker1 from "../assets/parrotMarker1.png";
+import parrotMarker2 from "../assets/parrotMarker2.png";
+import parrotMarker3 from "../assets/parrotMarker3.png";
+import parrotMarker4 from "../assets/parrotMarker4.png";
+import parrotMarker5 from "../assets/parrotMarker5.png";
+import parrotMarker6 from "../assets/parrotMarker6.png";
 
 export const ClusteredVoyageMarkers = ({ voyages }) => {
   const [markers, setMarkers] = useState({});
@@ -15,8 +21,6 @@ export const ClusteredVoyageMarkers = ({ voyages }) => {
     [voyages, selectedVoyageKey]
   );
 
-  // Create the markerClusterer once the map is available and update it when
-  // the markers are changed
   const map = useMap();
   const clusterer = useMemo(() => {
     if (!map) return null;
@@ -73,33 +77,111 @@ export const ClusteredVoyageMarkers = ({ voyages }) => {
           lng: voyage.waypoints[0].longitude,
         };
 
-        const marker = new window.google.maps.Marker({
+        let markerName =
+          index % 6 === 0
+            ? parrotMarker1
+            : index % 6 === 1
+            ? parrotMarker2
+            : index % 6 === 2
+            ? parrotMarker3
+            : index % 6 === 3
+            ? parrotMarker4
+            : index % 6 === 4
+            ? parrotMarker5
+            : parrotMarker6;
+
+        const parrotMarkerImg = document.createElement("img");
+        parrotMarkerImg.src = markerName;
+        parrotMarkerImg.style.width = "3rem";
+        parrotMarkerImg.style.height = "4rem";
+
+        const marker = new window.google.maps.marker.AdvancedMarkerElement({
           position,
-          title: voyage.name, // Optional: Set the marker's title
+          title: voyage.id.toString(),
+          content: parrotMarkerImg,
+          gmpClickable: true,
         });
 
-        marker.addListener("click", () =>
-          handlePanToLocation(position.lat, position.lng)
-        );
+        const parrotMarkerImg2a = document.createElement("img");
+        parrotMarkerImg2a.src = markerName;
+        parrotMarkerImg2a.style.width = "15rem";
+        parrotMarkerImg2a.style.height = "20rem";
+        const parrotMarkerImg2b = document.createElement("img");
+        parrotMarkerImg2b.src = parrotMarker1;
+        parrotMarkerImg2b.style.width = "5rem";
+        parrotMarkerImg2b.style.height = "14rem";
+        const parrotDiv2 = document.createElement("div");
+        parrotDiv2.appendChild(parrotMarkerImg2a);
+        parrotDiv2.appendChild(parrotMarkerImg2b);
+
+        const parrotContent = () => {
+          return (
+            <>
+              {voyages.map((voyage, index) => {
+                return (
+                  voyage.waypoints?.[0] && (
+                    <div key={`${voyage.waypoints[0].latitude}-${index}`}>
+                      <MarkerWithInfoWindow
+                        index={index}
+                        position={{
+                          lat: voyage.waypoints[0].latitude,
+                          lng: voyage.waypoints[0].longitude,
+                        }}
+                        voyage={voyage}
+                        onClick={() =>
+                          handlePanToLocation(
+                            voyage.waypoints[0].latitude,
+                            voyage.waypoints[0].longitude
+                          )
+                        }
+                      />
+                    </div>
+                  )
+                );
+              })}
+            </>
+          );
+        };
+
+        const infoWindow = new window.google.maps.InfoWindow({
+          content: parrotDiv2,
+        });
+        let isInfoWindowOpen = false;
+
+        marker.addListener("click", () => {
+          if (isInfoWindowOpen) {
+            infoWindow.close();
+          } else {
+            infoWindow.open({
+              anchor: marker,
+              map,
+            });
+          }
+          isInfoWindowOpen = !isInfoWindowOpen; // Toggle the state
+        });
 
         setMarkerRef(marker, `${voyage.waypoints[0].latitude}-${index}`);
 
         return marker;
       })
-      .filter((marker) => marker !== null); // Remove null entries (where no waypoints exist)
+      .filter((marker) => marker !== null);
   }, [voyages, handlePanToLocation, setMarkerRef]);
 
-  // Add the markers to the clusterer whenever they change
   useEffect(() => {
     if (!clusterer || voyageMarkers.length === 0) return;
 
     clusterer.clearMarkers();
     clusterer.addMarkers(voyageMarkers);
+
+    const clustersWithIds = clusterer.clusters.map((cluster) =>
+      cluster.markers.map((marker) => marker.id)
+    );
+    console.log("clustersWithIds.", clustersWithIds);
   }, [clusterer, voyageMarkers]);
 
   return (
     <>
-      {voyages.map((voyage, index) => {
+      {/* {voyages.map((voyage, index) => {
         return (
           voyage.waypoints?.[0] && (
             <div key={`${voyage.waypoints[0].latitude}-${index}`}>
@@ -120,16 +202,7 @@ export const ClusteredVoyageMarkers = ({ voyages }) => {
             </div>
           )
         );
-      })}
-
-      {selectedVoyageKey && (
-        <InfoWindow
-          anchor={markers[selectedVoyageKey]}
-          onCloseClick={handleInfoWindowClose}
-        >
-          {selectedVoyage?.name}
-        </InfoWindow>
-      )}
+      })} */}
     </>
   );
 };
